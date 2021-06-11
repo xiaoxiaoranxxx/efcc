84fc33101f7d9414477891ad430549b7d723ee1f
xiaoxiaoran <2425940130@qq.com>
2021-06-11 22:03:20 +0800

    基本数据类型

--- xiaoxiaoran1/xiao1/hello.java
+++ xiaoxiaoran1/xiao1/hello.java
@@ -1,10 +1,29 @@
-// Hello.java
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
+/*
+** Hello.java
+** xiaoxiaoran
+*/
+
+public class hello { // 定义类
+    public static void main(String[] args) { // main方法,程序起点
+
+        System.out.println("肖萧然最秀...\nhello world");
+        System.out.println(3.1444545);
+        System.out.println('A');
+        System.out.println(false);
+
+        int x1 = 12;
+        float x2 = 1.1111F;
+        byte x3 = 11;
+        long x4=888888888888888888L;
+
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        System.out.println(x4);
+
     }
 
-    // JVM  java virtual machine 翻译作用
+    // JVM java virtual machine 翻译作用
     // JRE java runtime environment 运行环境
     // JDK java development kit 开发工具包
 
