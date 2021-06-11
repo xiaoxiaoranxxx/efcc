5933df4e9fc64b07df5e5c7bd7cc1866671266ff
xiaoxiaoran <2425940130@qq.com>
2021-06-11 21:12:16 +0800

    解决中文乱码

--- xiaoxiaoran1/xiao1/hello.java
+++ xiaoxiaoran1/xiao1/hello.java
@@ -3,4 +3,9 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+
+    // JVM  java virtual machine 翻译作用
+    // JRE java runtime environment 运行环境
+    // JDK java development kit 开发工具包
+
 }
