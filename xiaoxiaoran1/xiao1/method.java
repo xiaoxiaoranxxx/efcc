fa22d89cff3a048eb091186f025c84affa808fbb
xiaoxiaoran <2425940130@qq.com>
2021-06-14 22:49:23 +0800

    return

--- xiaoxiaoran1/xiao1/method.java
+++ xiaoxiaoran1/xiao1/method.java
@@ -2,12 +2,22 @@ public class method {
     public static void main(String[] args) {
         xiao();
 
+        // xunhuan();
+
+        System.out.print(sum(1, 2));
+
+        int num = sum(11, 22);
+        System.out.println(num + "秀秀");
+
+    }
+
+    public static void xunhuan() {
         int money = 1;
         while (true) {
             for (int i = 0; i <= 8; i++) {
                 if (money >= 1000) {
                     System.out.println("ok");
-                    if(money>1222)
+                    if (money > 1222)
                         break;
                 } else if (money > 500)
                     System.out.println("5555");
@@ -23,5 +33,9 @@ public class method {
 
     public static void xiao() {
         System.out.println("xiaoxiaoran666");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
