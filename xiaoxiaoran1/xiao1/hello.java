6ca171d80a389e48d8ca92aa359e77fd9cdd87e7
xiaoxiaoran <2425940130@qq.com>
2021-07-28 16:42:27 +0800

    java16

--- xiaoxiaoran1/xiao1/hello.java
+++ xiaoxiaoran1/xiao1/hello.java
@@ -2,13 +2,17 @@
 ** Hello.java   
 ** xiaoxiaoran
 ** cd "c:\Users\Administrator\efcc\xiaoxiaoran1\xiao1\" ; if ($?) { javac -encoding utf-8 hello.java } ; if ($?) { java hello }
+**
+**      CLASSPATH -> .;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar;
+**      JAVA_HOME -> D:\Program Files\Java\jdk-16.0.2
+**      JAVA_HOME -> C:\Program Files\Java\jdk1.8.0_191
+**      %JAVA_HOME%\bin
+**      %JAVA_HOME%\jre\bin
 */
-
 
 // JVM java virtual machine 翻译作用
 // JRE java runtime environment 运行环境
 // JDK java development kit 开发工具包
-
 
 public class hello { // 定义类
     public static void main(String[] args) { // main方法,程序起点
@@ -33,9 +37,8 @@ public class hello { // 定义类
 
         String x6 = "xiaoxiaoran";
         System.out.println(x6 + "zuixiu"); // xiaoxiaoranzuixiu
-        System.out.println(x6 + 2222);  // xiaoxiaoran2222
-        
-    }
+        System.out.println(x6 + 2222); // xiaoxiaoran2222
 
+    }
 
 }
