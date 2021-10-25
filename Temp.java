c3cb2fb1a37b3538e673eb075d5bdff7f3234d3d
xiaoxiaoran <2425940130@qq.com>
2021-10-25 22:59:00 +0800

    作业

--- Temp.java
+++ Temp.java
@@ -1,17 +1,104 @@
-import java.util.*;
+class Person {
+    private String name;
+    private String cardid;
+    private int power;
+
+    public Person() {
+    }
+
+    public Person(String name, String cardid) {
+        this.name = name;
+        this.cardid = cardid;
+    }
+
+    public Person(String name, String cardid, int power) {
+        this.name = name;
+        this.cardid = cardid;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardid() {
+        return cardid;
+    }
+
+    public void setCardid(String cardid) {
+        this.cardid = cardid;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void output() {
+        System.out.println("姓名： " + name);
+        System.out.println("身份证： " + shen(cardid));
+
+        if (lengtVerify(cardid)) {
+            System.out.println("出生日期： " + chu(cardid));
+            System.out.println("性别： " + checksex(cardid));
+        }
+        System.out.println("体力 " + power);
+    }
+
+    public static String chu(String s) {
+        String x = s.substring(6, 10) + "年" + s.substring(10, 12) + "月" + s.substring(12, 14) + "日";
+        return x;
+    }
+
+    public static boolean lengtVerify(String s) {
+        if (s.length() == 18)
+            return true;
+        return false;
+    }
+
+    public static String shen(String s) {
+        if (lengtVerify(s))
+            return s;
+        return "false-->xxx";
+    }
+
+    public static String checksex(String s) {
+        if ((int) s.charAt(16) % 2 == 1)
+            return "boy";
+        return "girl";
+    }
+
+    public void sleep() {
+        this.power += 1;
+    }
+
+    public void run() {
+        this.power -= 1;
+    }
+
+}
 
 public class Temp {
+
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<Integer, String>(); // 创建Map集合
-        map.put(1, "张三"); // 存储键和值
-        map.put(2, "李四");
-        map.put(3, "王五");
-        Set<Integer> keySet = map.keySet(); // 获取键的集合
-        Iterator<Integer> it = keySet.iterator(); // 迭代键的集合
-        while (it.hasNext()) {
-            Object key = it.next();
-            Object value = map.get(key); // 获取每个键所对应的值
-            System.out.println(key + ":" + value);
-        }
+
+        Person xiao = new Person("xiaoxiaoran", "412825200002242113");
+        xiao.output();
+        System.out.println();
+        Person xiao1 = new Person("肖萧然", "1234554545454546789", 10);
+        xiao1.output();
+        xiao1.run();
+        System.out.println(xiao1.getPower());
+        xiao1.sleep();
+        System.out.println(xiao1.getPower());
+
     }
+
 }
