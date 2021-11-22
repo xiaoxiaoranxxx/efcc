f3fa0562012c546b32ee4b5329b8de8debeec551
xiaoxiaoran <2425940130@qq.com>
2021-11-22 11:42:54 +0800

    关闭

--- xiaoxiaoran3/mysql/Sql.java
+++ xiaoxiaoran3/mysql/Sql.java
@@ -16,12 +16,20 @@ public class Sql {
             String sql = "select * from books";
             rs = stmt.executeQuery(sql);
             while (rs.next()) {
-                System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "-->价格$" + rs.getString(3));
+                System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "-->价格$" + rs.getString("author"));
             }
 
         } catch (ClassNotFoundException e) {
 
             e.printStackTrace();
+        } finally {
+            if (rs != null)
+                rs.close();
+            rs = null;
+            stmt.close();
+            stmt = null;
+            conn.close();
+            conn = null;
         }
 
     }
