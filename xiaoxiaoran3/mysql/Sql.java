2e543adedd32dc0268642add522ebdf65bcf605d
xiaoxiaoran <2425940130@qq.com>
2021-11-22 11:36:25 +0800

    数据库连接

---
+++ xiaoxiaoran3/mysql/Sql.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class Sql {
+    public static void main(String[] args) throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1:3306/xiaoxiaoran";
+            String username = "root";
+            String password = "root";
+            conn = DriverManager.getConnection(url, username, password);
+            stmt = conn.createStatement();
+            String sql = "select * from books";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "-->价格$" + rs.getString(3));
+            }
+
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
